e738960a9b9c6c67ac2aafb736b9a304bc4e2493
irizzcodes <114680318+irizzcodes@users.noreply.github.com>
2022-10-23 02:05:39 +0800

    Add title panel and fix main and second panel

--- src/com/company/MyFrame.java
+++ src/com/company/MyFrame.java
@@ -8,11 +8,17 @@ public class MyFrame {
      MyFrame(){
         // set containers
         JPanel mainPanel = new JPanel();
-        mainPanel.setBackground(new Color(224, 202, 88));
+        mainPanel.setBackground(new Color(54, 54, 94));
+        mainPanel.setBounds(15,15,650,800);
+
         // set second container
          JPanel secondPanel = new JPanel();
-         secondPanel.setBackground(new Color(54, 54, 94));
-         mainPanel.setBounds(30,35,625,765);
+         secondPanel.setBackground(new Color(224, 202, 88));
+         // set third container "title of program"
+         JPanel titlePanel = new JPanel();
+         titlePanel.setBackground(new Color(224, 202, 88));
+         titlePanel.setSize(400,35);
+         titlePanel.setLocation(140, 50);
 
 
 
@@ -23,9 +29,10 @@ public class MyFrame {
         frame.setResizable(false);
         frame.setSize(700,875);
         frame.setVisible(true);
-        frame.getContentPane().setBackground(new Color(224, 202, 88));
+        frame.add(titlePanel);
         frame.add(mainPanel);
         frame.add(secondPanel);
+
 
 
 
