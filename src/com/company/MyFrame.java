48113301f802a8ad54ae2ef9e93955eedb8b2e83
irizzcodes <114680318+irizzcodes@users.noreply.github.com>
2022-10-23 02:13:50 +0800

    Add academic and personal panel

--- src/com/company/MyFrame.java
+++ src/com/company/MyFrame.java
@@ -19,6 +19,16 @@ public class MyFrame {
          titlePanel.setBackground(new Color(224, 202, 88));
          titlePanel.setSize(400,35);
          titlePanel.setLocation(140, 50);
+         // set Academic container
+         JPanel academicPanel = new JPanel();
+         academicPanel.setBackground(new Color(111, 141, 179));
+         academicPanel.setSize(525,120);
+         academicPanel.setLocation(80,140);
+         // set Personal container
+         JPanel personalPanel = new JPanel();
+         personalPanel.setBackground(new Color(111, 141, 179));
+         personalPanel.setSize(525,100);
+         personalPanel.setLocation(80,300);
 
 
 
@@ -29,6 +39,8 @@ public class MyFrame {
         frame.setResizable(false);
         frame.setSize(700,875);
         frame.setVisible(true);
+        frame.add(personalPanel);
+        frame.add(academicPanel);
         frame.add(titlePanel);
         frame.add(mainPanel);
         frame.add(secondPanel);
