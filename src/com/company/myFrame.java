6abbe03bb8e18804730409db9fe51c0f81f0d502
irizzcodes <114680318+irizzcodes@users.noreply.github.com>
2022-10-22 23:54:00 +0800

    Create constructor

--- src/com/company/myFrame.java
+++ src/com/company/myFrame.java
@@ -1,4 +1,16 @@
 package com.company;
 
-public class myFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class myFrame extends JFrame {
+
+    myFrame(){
+        this.setTitle("TIP Agents");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setSize(1000,1250);
+        this.setVisible(true);
+        this.getContentPane().setBackground(new Color(40, 39, 79));
+    }
 }
