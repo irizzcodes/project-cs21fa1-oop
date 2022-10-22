8e8173893041baa733c9334635e10f5c3151609e
irizzcodes <114680318+irizzcodes@users.noreply.github.com>
2022-10-23 01:40:31 +0800

    Add second panel

--- src/com/company/MyFrame.java
+++ src/com/company/MyFrame.java
@@ -3,22 +3,33 @@ package com.company;
 import javax.swing.*;
 import java.awt.*;
 
-public class MyFrame extends JFrame {
+public class MyFrame {
 
-    MyFrame(){
-        this.setTitle("TIP Agents");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setResizable(false);
-        this.setSize(700,875);
-        this.setVisible(true);
-        this.getContentPane().setBackground(new Color(219, 201, 110));
-
-
+     MyFrame(){
         // set containers
         JPanel mainPanel = new JPanel();
-        mainPanel.setBackground(new Color(40, 39, 79));
-    }
+        mainPanel.setBackground(new Color(224, 202, 88));
+        // set second container
+         JPanel secondPanel = new JPanel();
+         secondPanel.setBackground(new Color(54, 54, 94));
+         mainPanel.setBounds(30,35,625,765);
 
+
+
+        // main frame
+        JFrame frame = new JFrame();
+        frame.setTitle("TIP Agents");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(700,875);
+        frame.setVisible(true);
+        frame.getContentPane().setBackground(new Color(224, 202, 88));
+        frame.add(mainPanel);
+        frame.add(secondPanel);
+
+
+
+    }
 
 }
 
